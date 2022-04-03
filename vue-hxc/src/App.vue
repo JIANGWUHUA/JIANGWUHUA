dda10ea622b038537bfd906ae0e03b1fa7f969e7
hxc-pro <948598089@qq.com>
2022-04-04 03:24:43 +0800

    旺财俱乐部

---
+++ vue-hxc/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- 主页 -->
+    <my-index></my-index>
+    <!-- 购物车 -->
+    <my-wishlist></my-wishlist>
+  </div>
+</template>
+
+<script>
+import MyIndex from './components/MyIndex.vue'
+  export default {
+  components: { MyIndex,},
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+@import './assets/css/index.css'
+</style>
---
+++ vue-hxc/src/components/MyIndex.vue
@@ -0,0 +1,365 @@
+<template>
+  <div class="my-index">
+        <!-- 第一板块 -->
+<div class="nav_huo1">
+    <div class="banxin">  
+        <div class="huo_1dropdown">
+            <h3 class="huo_1btn">西安</h3>
+            <div class="dropdown_content">
+                <a href="#" class="dropdown_content_a">雁塔区</a>
+                <a href="#" class="dropdown_content_a">碑林区</a>
+                <a href="#" class="dropdown_content_a">长安区</a>
+                <a href="#" class="dropdown_content_a">未央区</a>
+                <a href="#" class="dropdown_content_a">高新区</a>
+                <a href="#" class="dropdown_content_a">西咸新区</a>
+            </div>
+        </div> 
+        
+        <ul>
+            <li><a href="#">登录</a></li>
+            <li><a href="#">注册</a></li>
+            <li><a href="#">联系客服</a></li>
+            <li><a href="#">关于我们</a></li>
+        </ul>
+    </div>
+</div>
+    <!-- 第二板块 -->
+<div class="nav_huo2">
+    <div class="banxin">
+        <div class="nav_huo2_1">
+            <a href="#"><img src="../assets/images/logo.jpg" alt="" class="logo"></a>
+            <input type="text" placeholder="西红柿/鸡蛋">
+            <a href="#"><button>搜索</button></a>
+            <ul>
+                <li><a href="#"><img src="../assets/images/icon/购物车.png" alt=""></a></li>
+                <li><a href="#">购物车</a></li>
+                <li><a href="#"><img src="../assets/images/user.png" alt=""></a></li>
+                <li><a href="#">&nbsp;江南</a></li>
+            </ul>
+        </div>
+        <div class="nav_bar">
+            <div class="banxin">
+                <!-- <span><hr></span> -->
+                <ul class="nav_f">
+                    <li class="dropword_1">
+                        <a href="#" class="dropbtn_1">Home</a>
+                        <div class="dropword_content_1">
+                            <a href="#">主页</a>
+                        </div>
+                    </li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">Category</a></li>
+                    <li><a href="#">Pages</a></li>
+                    <li><a href="#">Authentic</a></li>
+                </ul>
+                <ul class="nav_r">
+                    <li>
+                        <p><a href="#">&nbsp;huixuancai@wangcai.com</a></p>
+                    </li>
+                    <li>
+                        <a href="#"><img src="../assets/images/icon/邮箱.png" alt=""></a>
+                    </li>
+                    <li>
+                        <p><a href="#">&nbsp;156  9196  9361</a></p>
+                    </li>
+                    <li>
+                        <a href="#"><img src="../assets/images/icon/209电话-圆框.png" alt=""></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>  
+    <!-- 第三板块 -->
+<div class="banner_huo3">
+    <div class="pic">
+        <img src="../assets/images/home/category/01.jpg" alt="">
+        <img src="../assets/images/home/category/02.jpg" alt="">
+        <img src="../assets/images/home/category/03.jpg" alt="">
+    <!-- 切换按钮 -->
+        <div class="indicator">
+            <span data-i="0" class="active"></span>
+            <span data-i="1"></span>
+            <span data-i="2"></span>
+        </div>
+        <div class="btns">
+            <span></span>
+            <span></span>
+        </div>
+        <div class="btns_jiantou">
+
+        </div>
+    </div>
+</div>
+    <!-- 第四板块 -->
+<div class="special_huo4">
+    <div class="banxin">
+        <!-- <div>
+            <h1>SPECIAL DISCOUNT</h1>
+        </div> -->
+        <div class="donghua">
+            <ul class="donghua_ul">
+                <li>
+                    <a href="#"><img src="../assets/images/suggest/01.jpg" alt=""></a>
+                    <a href="#"><h4>vegetables</h4></a>
+                    <a href="#"><p>34 items</p></a>
+                </li>
+                <li>
+                    <a href="#"><img src="../assets/images/suggest/02.jpg" alt=""></a>
+                    <a href="#"><h4>fruits</h4></a>
+                    <a href="#"><p>89 items</p></a>
+                </li>
+                <li>
+                    <a href="#"><img src="../assets/images/suggest/03.jpg" alt=""></a>
+                    <a href="#"><h4>groceries</h4></a>
+                    <a href="#"><p>45 items</p></a>
+                </li>
+                <li>
+                    <a href="#"><img src="../assets/images/suggest/04.jpg" alt=""></a>
+                    <a href="#"><h4>dairy farm</h4></a>
+                    <a href="#"><p>83 items</p></a>
+                </li>
+                <li>
+                    <a href="#"><img src="../assets/images/suggest/05.jpg" alt=""></a>
+                    <a href="#"><h4>sea foods</h4></a>
+                    <a href="#"><p>40 items</p></a>
+                </li>
+                <li>
+                    <a href="#"><img src="../assets/images/suggest/06.jpg" alt=""></a>
+                    <a href="#"><h4>vegan foods</h4></a>
+                    <a href="#"><p>57 items</p></a>
+                </li>
+                <li>
+                    <a href="#"><img src="../assets/images/suggest/07.jpg" alt=""></a>
+                    <a href="#"><h4>dry foods</h4></a>
+                    <a href="#"><p>23 items</p></a>
+                </li>
+                <li>
+                    <a href="#"><img src="../assets/images/suggest/08.jpg" alt=""></a>
+                    <a href="#"><h4>fast foods</h4></a>
+                    <a href="#"><p>57 items</p></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+   
+</div>
+    <!-- 第五板块 -->
+<div class="new_Arrival">
+    <div class="banxin">
+        <div>
+            <a href="#"><h1>新品到货</h1></a>
+        </div>
+        <div class="up_line"></div>
+        <div class="new_kind">
+            <ul>
+                <li>
+                    <a href="#"><div class="new_huo5">&nbsp;NEW</div></a>
+                    <a href="#"><div class="aixin">❤</div></a>
+                    <a href="#"><img src="../assets/images/product/16.jpg" alt=""></a>
+                    <hr>
+                    <div class="wujiaoxin">★★★★★</div>
+                    <a href="#">
+                        <div class="dazhejia">
+                            <s>$12.99</s>
+                            <span>$6.99</span>
+                        </div></a>
+                    <a href="#"><button>立即抢购</button></a>
+                </li>
+                <li>
+                    <a href="#"><div class="new_huo5">&nbsp;NEW</div></a>
+                    <a href="#"><div class="aixin">❤</div></a>
+                    <a href="#"><img src="../assets/images/product/17.jpg" alt=""></a>
+                    <hr>
+                    <div class="wujiaoxin">★★★★★</div>
+                    <a href="#">
+                        <div class="dazhejia">
+                            <s>$19</s>
+                            <span>$15.8</span>
+                        </div></a>
+                    <a href="#"><button>立即抢购</button></a>
+                </li>
+                <li>
+                    <a href="#"><div class="new_huo5">&nbsp;NEW</div></a>
+                    <a href="#"><div class="aixin">❤</div></a>
+                    <a href="#"><img src="../assets/images/product/18.jpg" alt=""></a>
+                    <hr>
+                    <div class="wujiaoxin">★★★★★</div>
+                    <a href="#">
+                        <div class="dazhejia">
+                            <s>$8</s>
+                            <span>$5.9</span>
+                        </div></a>
+                    <a href="#"><button>立即抢购</button></a>
+                </li>
+                <li>
+                    <a href="#"><div class="new_huo5">&nbsp;NEW</div></a>
+                    <a href="#"><div class="aixin">❤</div></a>
+                    <a href="#"><img src="../assets/images/product/19.jpg" alt=""></a>
+                    <hr>
+                    <div class="wujiaoxin">★★★★★</div>
+                    <a href="#">
+                        <div class="dazhejia">
+                            <s>$22</s>
+                            <span>$13.5</span>
+                        </div></a>
+                    <a href="#"><button>立即抢购</button></a>
+                </li>
+                <li>
+                    <a href="#"><div class="new_huo5">&nbsp;NEW</div></a>
+                    <a href="#"><div class="aixin">❤</div></a>
+                    <a href="#"><img src="../assets/images/product/20.jpg" alt=""></a>
+                    <hr>
+                    <div class="wujiaoxin">★★★★★</div>
+                    <a href="#">
+                        <div class="dazhejia">
+                            <s>$34</s>
+                            <span>$25</span>
+                        </div></a>
+                    <a href="#"><button>立即抢购</button></a>
+                </li>
+            </ul>
+            <a href="#">
+                <div class="show_more">
+                        更多展示
+                </div></a>
+           
+        </div>
+    </div>
+</div> 
+    <!-- 第六板块 -->
+<div class="huo_6">
+    <div class="banxin_6">
+        <div>
+            <img src="../assets/images/newsletter.jpg" alt="">
+        </div>  
+        <div class="lvjing"></div>
+        <h1>Get 20% Discount For Subscriber</h1>
+        <input type="text" placeholder="Enter Your Email Address">
+        <button>@SUBSCRIBE</button>
+    </div>
+   
+</div> 
+    <!-- 第七板块 -->
+<div class="huo_7">
+    <div class="banxin">
+        <ul>
+            <li>
+                <img src="../assets/images/icon/wuliu.png" alt="">
+                <h4>免费送货上门</h4>
+                <p>配送过程中至少与客户沟通一次,通知准确到达时间,避免不必要的等候</p>
+            </li>
+            <li>
+                <img src="../assets/images/icon/tuihuan.png" alt="">
+                <h4>即时退货政策</h4>
+                <p>24小时的人工客服服务热线,第一时间处理您的不满意.</p>
+            </li>
+            <li>
+                <img src="../assets/images/icon/fuwu.png" alt="">
+                <h4>快速服务系统</h4>
+                <p>选菜有疑问,店小二帮您解决!</p>
+            </li>
+            <li>
+                <img src="../assets/images/icon/隐私保护.png" alt="">
+                <h4>安全支付方式</h4>
+                <p>坚持合规先行 监管合规 安全保障 全方位保障您的资金安全</p>
+            </li>
+        </ul>
+    </div>
+</div>
+    <!-- 第八板块 -->
+<div class="huo_8">
+    <div class="banxin">
+        <div class="huo_8_1">
+            <div>
+                <img src="../assets/images/logo.jpg" alt="">
+                <p>精选超4,000款商品,专业买手团队,只为把新鲜送到您的身边
+                    生鲜商品100%批次质检，拿到手 更安心!
+                    &nbsp;&nbsp;&nbsp;&nbsp;慧选菜---让每个家庭买得省心 吃得放心</p>
+                <p>慧选菜---让每个家庭买得省心 吃得放心</p>
+            </div>
+            <div>
+                <h1>关于我们</h1>
+                <img src="../assets/images/icon/邮箱 (1).png" alt="">
+                <p>huixuancai@wangcai.com</p>
+                <br>
+                <img src="../assets/images/icon/手机_sel.png" alt="">
+                <p>15691969361</p>
+                <br>
+                <img src="../assets/images/icon/位置.png" alt="">
+                <p>陕西省-西安市-碑林区-体育场</p>
+            </div>
+            <div>
+                <h1>配送服务</h1>
+                <span><a href="">配送流程</a></span>
+                <span><a href="">配送条件</a></span>
+                <span><a href="">分装中心</a></span>
+                <span><a href="">社区服务店</a></span>
+                <span><a href="">终端配送柜</a></span> 
+            </div>
+            <div>
+                <h1>24小时服务热线</h1>
+                <p>400-8888-8888</p>
+                <img src="../assets/images/app-store.png" alt="">
+                <img src="../assets/images/google-store.png" alt="">
+            </div>
+        </div>
+        
+        
+        <!-- 
+            <li>
+                
+            </li>
+        </ul> -->
+    </div> 
+</div>
+
+<div class="huo_8last">
+    <div class="banxin">
+        <!-- <div class="last_fuwu">
+            <ul>
+                <li>
+                    <img src="./icon/营养服务.png" alt="">
+                    <span>营养服务</span>
+                </li>
+                <li>
+                    <img src="./icon/专业配送.png" alt="">
+                    <span>专业配送</span>
+                </li>
+                <li>
+                    <img src="./icon/品质保证.png" alt="">
+                    <span>品质保证</span>
+                </li>
+                <li>
+                    <img src="./icon/买贵补差.png" alt="">
+                    <span>买贵补差</span>
+                </li>
+                <li>
+                    <img src="./icon/增值服务.png" alt="">
+                    <span>增值服务</span>
+                </li>
+            </ul>
+        </div> -->
+        <div class="huo_8last_1">
+            <p>© All Copyrights Reserved by wangcaijulebu">Mironcoder</p>         
+            <div class="huo_8last_2">
+            <a href="#"><img src="../assets/images/payment/jpg/01.jpg" alt="" ></a>
+            <a href="#"><img src="../assets/images/payment/jpg/02.jpg" alt="" ></a>
+            <a href="#"><img src="../assets/images/payment/jpg/03.jpg" alt="" ></a>
+            <a href="#"><img src="../assets/images/payment/jpg/04.jpg" alt="" ></a>
+            </div>         
+        </div>
+    </div>
+</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
